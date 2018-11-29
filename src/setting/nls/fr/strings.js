3fe836d32d78713223b275b7e0a4ea0a4949b442
Gijs Boekema <gboekema@cyclomedia.com>
2018-11-29 13:43:02 +0100

    WIDGET-64 fix for french languages

--- src/setting/nls/fr/strings.js
+++ src/setting/nls/fr/strings.js
@@ -1,15 +1,15 @@
 ﻿define(
    ({
        username: "Nom d'utilisateur",
-       password: "mot de passe",
+       password: "Mot de passe",
        locale: "lieu",
-       srs: "Viewer SRS",
+       srs: "SRS",
        agreement1: "je suis d'accord avec les",
        agreement2: "Termes et conditions de l'utilisateur pour Street Smart.",
        headAgreement: "Accord de l'utilisateur",
        measure: "Mesures",
        overlay: "Superpositions",
-       navigation: "La navigation",
+       navigation: "Navigation",
        typeSrs: "Tapez ou sélectionnez SRS",
        version: "Street Smart Widget pour Web AppBuilder @ version 18.2"
   })
