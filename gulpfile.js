cf306ee81c82577f529714a3f6192e41ad05377a
gpcboekema <gboekema@cyclomedia.com>
2019-10-09 09:58:18 +0200

    STREET-2439 Upgrading gulp (#44)
    * STREET-2439 upgrading gulp and fixing gulpfile
    
    * STREET-2439 start liverealod

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,37 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-var livereload = require('gulp-livereload');
+const gulp = require('gulp');
+const babel = require('gulp-babel');
+const livereload = require('gulp-livereload');
 require('dotenv').config();
 
 console.log('WIDGET DIRECTORY: ', process.env.WIDGET_DIR);
 
-gulp.task('babel', function() {
-    return gulp.src([
-        'src/**/*.js', '!**/*___jb_old___'
-    ])
-        .pipe(babel())
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest(process.env.WIDGET_DIR))
-        .pipe(livereload());
-});
+const babelify = () => gulp.src(['src/**/*.js', '!**/*___jb_old___'])
+    .pipe(babel())
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(process.env.WIDGET_DIR))
+    .pipe(livereload());
 
-gulp.task('copy', function() {
-    // Also copy all non javascript files to the dist folder
-    gulp.src(['src/**/*.*', '!src/**/*.js', '!**/*___jb_old___'], { base: 'src' })
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest(process.env.WIDGET_DIR));
+const copyAssets = () => gulp.src(['assets/**/*.*', '!**/*___jb_old___'], { base: 'assets' })
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(process.env.WIDGET_DIR))
+    .pipe(livereload());
 
-    gulp.src(['assets/**/*.*', '!**/*___jb_old___'], { base: 'assets' })
-        .pipe(gulp.dest('dist'))
-        .pipe(gulp.dest(process.env.WIDGET_DIR))
-        .pipe(livereload());
-});
 
-gulp.task('default', ['babel', 'copy']);
+const copySource = () => gulp.src(['src/**/*.*', '!src/**/*.js', '!**/*___jb_old___'], { base: 'src' })
+    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(process.env.WIDGET_DIR));
 
-gulp.task('watch', function() {
+// Also copy all non javascript files to the dist folder
+const copy = done => gulp.parallel(copySource, copyAssets)(done);
+
+const main = done => gulp.series(babelify, copy)(done);
+
+const watch = () => {
     livereload.listen();
-    gulp.watch(['src/**/*.*', 'assets/**/*.*'], ['default']);
-});
+    gulp.watch(['src/**/*.*', 'assets/**/*.*'], main);
+}
+
+exports.default = main;
+exports.babel = babelify;
+exports.copy = copy;
+exports.watch = watch;
