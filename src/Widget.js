a798f6b47f16a767be799eecb3a859c27c75b92f
Giorgy <gcastaneda@cyclomedia.com>
2021-12-21 15:12:59 -0500

    STREET-4067, Changed file paths for local packages

--- src/Widget.js
+++ src/Widget.js
@@ -2,10 +2,10 @@ const REQUIRE_CONFIG = {
     async: true,
     locale: 'en',
     paths: {
-        'react': './packages/react.production.min',
-        'react-dom': './packages/react-dom.production.min',
-        'openlayers': './packages/ol.min',
-        'lodash': './packages/lodash.min'
+        'react': '/../widgets/StreetSmart/packages/react.production.min',
+        'react-dom': '/../widgets/StreetSmart/packages/react-dom.production.min',
+        'openlayers': '/../widgets/StreetSmart/packages/ol.min',
+        'lodash': '/../widgets/StreetSmart/packages/lodash.min'
     }
 };
 
