ed11203e5733561ed65a5ecf9c27727a6d06f400
Giorgy <gcastaneda@cyclomedia.com>
2022-01-12 15:44:22 -0500

    STREET-4134: Updated code to allow measurements to be saved

--- src/utils.js
+++ src/utils.js
@@ -34,6 +34,7 @@ define([
 
             //No transformation needed if source SRS == target SRS
             if (sourceGeom.spatialReference.wkid === targetSrs) {
+                //this will return a new saved measurement
                 return sourceGeom;
             }
 
@@ -47,9 +48,20 @@ define([
             }
 
             var source = this.proj4(sourceEpsg);
-            var dest = this.proj4(destEpsg);
+            //var dest = this.proj4(destEpsg);
+            //looks for an error exception if the proj4 function cannot find the destination projection
+            //if error is caught, then the destination projection is the same as the source projection
+            try{
+                var dest = this.proj4(destEpsg);
+            }
+            catch(err){
+                //var dest = source;
+                alert("Editable layer SRS (Spatial Reference System) does not match Street Smart SRS: Please make them match to save measurements.");
+                var dest = this.proj4(destEpsg);
+            }
 
             var p = this.proj4(source, dest).forward([sourceGeom.x, sourceGeom.y]);
+            //this will NOT return a new saved measurement
             return new Point(p[0], p[1], new SpatialReference({ wkid: parseInt(targetSrs) }));
         }
     };
